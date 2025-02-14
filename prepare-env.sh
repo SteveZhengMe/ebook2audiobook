8f241822657db5f120e2c149fd0cab13107af701
Steve <steve.zheng@gmail.com>
2025-02-14 15:42:29 -0500

    Remove Chinese comments.

--- prepare-env.sh
+++ prepare-env.sh
@@ -9,12 +9,12 @@ sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | 
 sudo apt-get install -y ffmpeg
 apt-get install -y mecab libmecab-dev mecab-ipadic-utf8
 
-# 判断有没有venv环境，没有就新建一个
+
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
 fi
 
-# 判断当前是否已经激活venv，没有的话，激活
+
 if [ -z "$VIRTUAL_ENV" ]; then
     source .venv/bin/activate
 fi
--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-# 判断当前是否已经激活venv，没有的话，激活
 if [ -z "$VIRTUAL_ENV" ]; then
     source .venv/bin/activate
 fi
 
-# 如果传入可参数share，则运行python app.py --share；否则运行python app.py
 if [ "$1" = "share" ]; then
     python3 app.py --share
 else
